f87642c1ebfb147b57aae24fb89ad3d0b65bd093
v833 <2507301541@qq.com>
2022-01-13 00:13:46 +0800

    SchemeForm骨架搭建

---
+++ libs/SchemaForm.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '',
+  setup(props, { slots, emit, attrs }) {
+    return () => {
+      return <div>This is Form</div>
+    }
+  }
+})
