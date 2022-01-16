6694303a5c64ff4733ac08509fa2e3274f8d4277
v833 <2507301541@qq.com>
2022-01-16 17:31:40 +0800

    ObjectField的单元测试

--- libs/index.ts
+++ libs/index.ts
@@ -1,6 +1,8 @@
 import SchemaForm from './SchemaForm'
 import NumberField from './fields/NumberField'
 import StringField from './fields/StringField'
+import ObjectField from './fields//ObjectField'
+import ArrayField from './fields/ArrayField'
 export default SchemaForm
 
-export { NumberField, StringField }
+export { NumberField, StringField, ObjectField, ArrayField }
---
+++ tests/unit/ObjectField.spec.ts
@@ -0,0 +1,71 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import JsonSchemaForm, {
+  ObjectField,
+  StringField,
+  NumberField
+} from '../../libs'
+
+describe('ObjectField', () => {
+  let schema: any = undefined
+  beforeEach(() => {
+    schema = {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        age: {
+          type: 'number'
+        }
+      }
+    }
+  })
+  it('should render properties to correct fields', async () => {
+    const wrapper = mount(JsonSchemaForm as any, {
+      props: {
+        schema,
+        value: {},
+        onChange: () => ({})
+      }
+    })
+    const strField = wrapper.findComponent(StringField)
+    const numField = wrapper.findComponent(NumberField)
+    expect(strField.exists()).toBeTruthy()
+    expect(numField.exists()).toBeTruthy()
+  })
+  it('should change value when sub fields trigger onChange', async () => {
+    let value: any = {}
+    const wrapper = mount(JsonSchemaForm as any, {
+      props: {
+        schema,
+        value,
+        onChange: (v: any) => {
+          value = v
+        }
+      }
+    })
+    const strField = wrapper.findComponent(StringField)
+    const numField = wrapper.findComponent(NumberField)
+    await numField.props('onChange')(1)
+    expect(value.age).toEqual(1)
+    await strField.props('onChange')('1')
+    expect(value.name).toEqual('1')
+  })
+  it('should change value when sub fields trigger onChange (undefined)', async () => {
+    let value: any = {
+      name: '123'
+    }
+    const wrapper = mount(JsonSchemaForm as any, {
+      props: {
+        schema,
+        value,
+        onChange: (v: any) => {
+          value = v
+        }
+      }
+    })
+    const strField = wrapper.findComponent(StringField)
+    await strField.props('onChange')(undefined)
+    expect(value.name).toBeUndefined()
+  })
+})
