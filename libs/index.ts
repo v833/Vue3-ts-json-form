0e8d44df9af5ec2c9c4c5cd3c551d38490a5b08e
v833 <2507301541@qq.com>
2022-01-16 18:06:28 +0800

    update jest

--- libs/index.ts
+++ libs/index.ts
@@ -3,6 +3,7 @@ import NumberField from './fields/NumberField'
 import StringField from './fields/StringField'
 import ObjectField from './fields//ObjectField'
 import ArrayField from './fields/ArrayField'
+import SelectionWidget from './widgets/Selection'
 export default SchemaForm
 
-export { NumberField, StringField, ObjectField, ArrayField }
+export { NumberField, StringField, ObjectField, ArrayField, SelectionWidget }
